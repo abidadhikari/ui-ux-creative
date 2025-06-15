751e416c6f1d075c3390e19a260e9315f1d04ae9
Abid Adhikari <abidadhikari@gmail.com>
2025-06-15 15:55:41 +0545

    added missing start now link

--- src/app/(productpage)/layout.tsx
+++ src/app/(productpage)/layout.tsx
@@ -12,9 +12,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Link href="/">
             <Image src={arrowImage} alt="arrow" className="rotate-180" />
           </Link>
-          <button className="fixed top-[40px] right-0 button-gradient text-white py-4 px-11 rounded-[100px_0_0_100px] font-bold backdrop-blur-2xl">
+          <Link
+            href="https://wa.me/9779813270713?text=Hello%2C%20I%20would%20like%20to%20know%20more%20about%20your%20services"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fixed top-[40px] right-0 button-gradient text-white py-4 px-11 rounded-[100px_0_0_100px] font-bold backdrop-blur-2xl"
+          >
             Start New Project
-          </button>
+          </Link>
         </div>
         {children}
       </div>
